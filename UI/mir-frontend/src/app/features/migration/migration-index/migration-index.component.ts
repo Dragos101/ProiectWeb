a65d7fb9c0a2b7e461e3dfa9c3ea27b73ee7744d
Dragos <57534351+Dragos101@users.noreply.github.com>
2024-01-27 17:30:12 +0200

    init mapbox

--- UI/mir-frontend/src/app/features/migration/migration-index/migration-index.component.ts
+++ UI/mir-frontend/src/app/features/migration/migration-index/migration-index.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-migration-index',
   templateUrl: './migration-index.component.html',
   styleUrls: ['./migration-index.component.sass']
 })
-export class MigrationIndexComponent {
 
+export class MigrationIndexComponent implements OnInit {
+  map?: mapboxgl.Map
+
+  constructor() {
+    (mapboxgl as any).accessToken = environment.mapBoxApi
+  }
+
+  ngOnInit(): void {
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      zoom: 6,
+      center: [24.9668, 45.9432],
+    })
+
+    const popup = new mapboxgl.Popup().setHTML(
+      `<h3>Migratie 1</h3><p>Pasarile calatoare pe la cuiburi se aduna</p>`
+    );
+
+    new mapboxgl.Marker()
+      .setLngLat([24.9668, 45.9432])
+      .setPopup(popup)
+      .addTo(this.map);
+  }
 }
+
