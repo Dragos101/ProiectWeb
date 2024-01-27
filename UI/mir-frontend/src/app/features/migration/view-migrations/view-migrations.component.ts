2834092da9899024a1c6a2bb3ff7124aab6fcd43
Dragos <57534351+Dragos101@users.noreply.github.com>
2024-01-27 13:29:54 +0200

    Nits UI

--- UI/mir-frontend/src/app/features/migration/view-migrations/view-migrations.component.ts
+++ UI/mir-frontend/src/app/features/migration/view-migrations/view-migrations.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-view-migrations',
   templateUrl: './view-migrations.component.html',
   styleUrls: ['./view-migrations.component.sass']
 })
-export class ViewMigrationsComponent {
+export class ViewMigrationsComponent implements OnInit {
+  migrations: boolean = false
+  description: string = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum molestiae animi, explicabo nesciunt nobis qui soluta quia repellat tenetur et repellendus est, voluptatum expedita ullam, ut a aperiam ratione rem?'
+  shortDescription?: string
+
+  showLessDescription() {
+    return this.description.length > 100
+  }
+
+  hasMigrations() {
+    return this.migrations !== false
+  }
+
+  ngOnInit(): void {
+    this.shortDescription = this.description.slice(0,150)
+  }
 
 }
