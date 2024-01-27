15f11674aa6942e0ec98a9df1a782828255458fe
Dragos <57534351+Dragos101@users.noreply.github.com>
2024-01-27 13:55:40 +0200

    added mapbox and key

--- UI/mir-frontend/src/environments/environment.development.ts
+++ UI/mir-frontend/src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  apiBaseUrl: 'http://localhost:5026'
+  apiBaseUrl: 'http://localhost:5026',
+  mapBoxApi: 'pk.eyJ1IjoiZHJhZ29zMTAxIiwiYSI6ImNscncwN2ZmYzBwb3IycW82bHpvM294anEifQ.lw6Q1zK6GkLmt2pWiwlLgg'
 };
